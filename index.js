6c01fbf5d088f4265191461de3cc431f14f1369d
PortalFan202 <anibaldonoso1@gmail.com>
2022-12-11 22:09:06 +0200

    Changed the way the app listens

--- index.js
+++ index.js
@@ -167,16 +167,20 @@ app.get("/", function (req, res) {
     res.send("hello world");
 });
 
-https
-    .createServer(
-        {
-            key: fs.readFileSync("server.key"),
-            cert: fs.readFileSync("server.cert"),
-        },
-        app
-    )
-    .listen(3000, function () {
-        console.log(
-            "Example app listening on port 3000! Go to https://localhost:3000/"
-        );
-    });
+// https
+//     .createServer(
+//         {
+//             key: fs.readFileSync("server.key"),
+//             cert: fs.readFileSync("server.cert"),
+//         },
+//         app
+//     )
+//     .listen(3000, function () {
+//         console.log(
+//             "Example app listening on port 3000! Go to https://localhost:3000/"
+//         );
+//     });
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
